bf45bbee636c052d4e1a9609dd0e89d9adaa1a2c
Joel Elfrink <joelelfrink@gmail.com>
2016-04-26 22:20:30 -0600

    new responsive function

--- sites/kelsSite/js/responsive.js
+++ sites/kelsSite/js/responsive.js
@@ -1,16 +1,37 @@
 $(function() {
 
+	var active = false;
+
 	$('#menuButton').click(function(){
 		$('#nav').toggle('slide', {direction:'right'}, 'slow');
+		if (active == false) {
+			active = true;
+		}
+		else if (active == true) {
+			active = false;
+		}
 	});
 
 	$(window).resize(function(){
-		if ($(window).width()<769 && $('#nav').css('display') == 'table-cell') {
-			$('#nav').css({display: 'none'});
+		if (active == false) {
+			if ($(window).width()>768) {
+				$('#nav').css({display: 'table-cell'});
+			}
+			if ($(window).width()<=768) {
+				$('#nav').css({display: 'none'});
+			}
 		}
-		else  {
-			$('#nav').css({display: 'table-cell'});
-		};
+		else if (active == true) {
+			if ($(window).width()>768) {
+				$('#nav').css({display: 'table-cell'});
+				active = false;
+			}
+			if ($('#nav').css('display') == 'none') {
+				$('#nav').css({display: 'none'});
+			}
+			if ($('#nav').css('display') == 'block') {
+				$('#nav').css({display: 'block'});
+			}
+		}
 	});
-
 });
